e64c52c684cdfa0b41401f05881160208f0630bb
Zealll <eriznis@gmail.com>
2019-11-03 22:04:32 -0500

    refractoring

--- portfolio/src/Header.js
+++ portfolio/src/Header.js
@@ -14,24 +14,42 @@ import Arrow from "./assets/Arrow.png"
 export default function Header() {
     return (
         <div className="header">
-            <div className="top-left"><img src={TopLeft}></img></div>
-            <div className="top-middle"><img src={TopMiddle}></img></div>
-            <div className="top-right"><img src={TopMiddleRight}></img></div>
-            <div className="middle-left">
-                <div className="layer-one">
-                <p>Hi, my name is Kerry<br></br> McPhearson & I’m a <br></br>  Product Designer Hailing <br></br> From Atlanta, GA</p>
+            <div className='top-img-container'>
+                <div className="top-left"><img src={TopLeft}></img></div>
+                <div className="top-middle"><img src={TopMiddle}></img></div>
+                <div className="top-right"><img src={TopMiddleRight}></img></div>
+            </div>
+            
+            <div className='middle-img-container'>
+                <div className="middle-left">
+                    <div className="layer-one">
+                        <p>Hi, my name is Kerry
+                        <br />
+                        McPhearson & I’m a 
+                        <br />
+                        Product Designer Hailing 
+                        <br />
+                        From Atlanta, GA</p>
+                    </div>
+                    {/* <div className="layer-two">
+                        <img src={MiddleLeft}></img>
+                    </div> */}
                 </div>
-                <div className="layer-two">
-                <img src={MiddleLeft}></img>
-                </div></div>
-
-            <div className="middle-front"><img src={MiddleFront}></img></div> 
-            <div className="middle-behind"><img src={MiddleBehind}></img></div> 
-            <div className="middle-right"><img src={MiddleRight}></img></div>
-            <div className="bottom-left"><img src={BottomLeft}></img></div>
-            <div className="bottom-middle"><img src={BottomMiddle}></img></div>
-            <div className="bottom-right"><img src={BottomRight}></img></div>
-            <div className="bottom-bottom"><a href="#Projects"><p className="small1">Click Here to View My Projects</p><img className="center" src={Arrow}></img></a></div>
+                <div className='profile-pic-container'>
+                    <div className="middle-front">
+                        <img src={MiddleFront} />
+                    </div> 
+                    <div className="middle-right"><img src={MiddleRight}></img></div>
+                </div>
+            </div>
+            
+            {/* <div className='bottom-img-container'>
+                <div className="bottom-left"><img src={BottomLeft}></img></div>
+                <div className="bottom-middle"><img src={BottomMiddle}></img></div>
+                <div className="bottom-right"><img src={BottomRight}></img></div>
+                <div className="bottom-bottom"><a href="#Projects"><p className="small1">Click Here to View My Projects</p><img className="center" src={Arrow}></img></a></div>
+            </div> */}
+            
         </div>
     )
 }
